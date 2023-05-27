f7e4e00904103b3b414d729ab198f7ef8f6b348b
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2023-05-27 17:31:09 -0300

    feat: consumo da api e listagem dos filmes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
-  return <div>Home</div>;
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const resp = await fetch(url);
+    const data = await resp.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRateURL = `${moviesURL}top_rated?${apiKey}`;
+    getTopRatedMovies(topRateURL);
+  }, []);
+
+  return (
+    <div>
+      {topMovies &&
+        topMovies.map((movie) => {
+          return <p key={movie.id}>{movie.title}</p>;
+        })}
+    </div>
+  );
 };
 
 export default Home;
