5cbbec046f46c7070fb4a41d6152af291941d194
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2023-05-27 20:16:35 -0300

    feat: implementação do buscar filmes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,39 @@
-import { Link } from "react-router-dom";
-import { SearchOutlined } from '@ant-design/icons';
+import { useState } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+import { SearchOutlined } from "@ant-design/icons";
 
 import "../components/Navbar.css";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit= (e) => {
+    e.preventDefault();
+
+    if(!search) return;
+
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  }
+
   return (
     <>
       <nav id="navbar">
         <h2>
           <Link to="/">LG.movies</Link>
         </h2>
-        <form>
-            <input type="text" placeholder="Busque por filmes" />
-            <button type="submit"><SearchOutlined /></button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Busque por filmes"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+          <button type="submit">
+            <SearchOutlined />
+          </button>
         </form>
       </nav>
     </>
