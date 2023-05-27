06b0d37fb33468cfc30db313a09a4955e613d084
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2023-05-27 19:46:05 -0300

    feat: finalização do estilo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import { SearchOutlined } from '@ant-design/icons';
 
+import "../components/Navbar.css";
+
 const Navbar = () => {
   return (
     <>
       <nav id="navbar">
         <h2>
-          <Link to="/">MoviesLib</Link>
+          <Link to="/">LG.movies</Link>
         </h2>
         <form>
             <input type="text" placeholder="Busque por filmes" />
